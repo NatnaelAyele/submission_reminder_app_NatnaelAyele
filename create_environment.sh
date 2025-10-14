bf05cd44ceb8eb696e5da9a57f0077aed9400ff0
NatnaelAyele <n.eticha@alustudent.com>
2025-10-14 17:17:45 +0200

    added error handling for user input and directory creation

--- create_environment.sh
+++ create_environment.sh
@@ -5,13 +5,23 @@ echo "Welcome!"
 read -p "Enter your name, please: " name
 parent_dir="submission_reminder_${name}"
 
+#check if name entered is not empty
+if [[ -z "$name" ]]; then
+  echo "Error: Name cannot be empty. Please run the script again." >&2
+  exit 1
+fi
 #create a main directory based on the name accepted from user and its following sub-directories
-mkdir -p $parent_dir/{app,modules,assets,config}
+#if the directories were not able to be created display error message and exit the program
+mkdir -p "$parent_dir"/{app,modules,assets,config} || { 
+    echo "Error: Unable to create directories in '$parent_dir' run the program agin." >&2
+    exit 1
+}
+
 
 # Create the reminder.sh file inside the 'app' directory
 # append a script to reminder.sh that loads configuration files and helper functions, displays assignment details, and runs the submission reminder check
 
-cat > $parent_dir/app/reminder.sh << 'DATA'
+cat > "$parent_dir/app/reminder.sh" << 'DATA'
 #!/bin/bash
 
 # Source environment variables and helper functions
@@ -32,9 +42,9 @@ DATA
 #create the function.sh file inside 'modules' directory
 #append a script to function.sh. The script defines a function that checks the submissions file and identifies students who have not submitted their assignments.
 #the script is taken from the resources provided in the assignment submission page
-cat > $parent_dir/modules/functions.sh << 'DATA'
+cat > "$parent_dir/modules/functions.sh" << 'DATA'
 #!/bin/bash
-#
+
 # Function to read submissions file and output students who have not submitted
 function check_submissions {
     local submissions_file=$1
@@ -58,7 +68,7 @@ DATA
 #append a data of student's name the assignment they're working on and the submission status for each student into submissions.txt
 #the data is taken from the resources provided in the assignment submission page
 
-cat > $parent_dir/assets/submissions.txt << 'DATA'
+cat > "$parent_dir/assets/submissions.txt" << 'DATA'
 student, assignment, submission status
 Chinemerem, Shell Navigation, not submitted
 Chiagoziem, Git, submitted
@@ -73,7 +83,7 @@ DATA
 
 # Create the config.env file inside the 'config' directory 
 # append a data to config.env. This data defines environment variables used by the reminder script.
-cat > $parent_dir/config/config.env << 'DATA'
+cat > "$parent_dir/config/config.env" << 'DATA'
 # This is the config file
 ASSIGNMENT="Shell Navigation"
 DAYS_REMAINING=2
@@ -81,7 +91,7 @@ DATA
 
 # Create the startup.sh script inside the main directory
 # append a script to startup.sh that runs the main reminder.sh script.
-cat > $parent_dir/startup.sh << 'DATA'
+cat > "$parent_dir/startup.sh" << 'DATA'
 #!/bin/bash
 bash ./app/reminder.sh
 DATA
@@ -91,4 +101,4 @@ find "$parent_dir" -type f -name "*.sh" -exec chmod +x {} \;
 
 #print a completion and exiting messages
 echo "Directory created, environment created successfully"
-echo "You can now go to ${parent_dir} and run the startup.sh file to start the application"
+echo "You can now go to \"$parent_dir\" and run the startup.sh file to start the application."
